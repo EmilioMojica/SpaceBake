6a92b94b8c2919f7be503ce0365e1d5b5aa3d6a7
Crucidian <Crucidian@users.noreply.github.com>
2019-01-18 12:37:26 -0500

    Adding all the functionality to the Atoms

--- SpaceBake/Assets/Anthony Santoro/Scripts/ControllerMovement.cs
+++ SpaceBake/Assets/Anthony Santoro/Scripts/ControllerMovement.cs
@@ -4,13 +4,86 @@ using UnityEngine;
 
 public class ControllerMovement : MonoBehaviour {
 
+    public GameObject isHeld, isTargetted;
+    public GameObject Hydrogen, Nitrogen, Oxygen, Carbon;
+    public GameObject selectPosition;
+
+    public Material hydrogenMat, nitrogenMat, carbonMat, oxygenMat;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        RaycastHit hit;
         transform.rotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTrackedRemote);
-	}
+
+        if (OVRInput.GetDown(OVRInput.Button.One))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                Debug.Log("You selected the " + hit.transform.name); // ensure you picked right object
+
+                if(hit.transform.name == "Hydrogen Sphere")
+                    isHeld = (GameObject)Instantiate(Hydrogen, new Vector3(2, 1, 3), Quaternion.identity);
+
+                if (hit.transform.name == "Nitrogen Sphere")
+                    isHeld = (GameObject)Instantiate(Nitrogen, new Vector3(2, 1, 3), Quaternion.identity);
+
+                if (hit.transform.name == "Oxygen Sphere")
+                    isHeld = (GameObject)Instantiate(Oxygen, new Vector3(2, 1, 3), Quaternion.identity);
+
+                if (hit.transform.name == "Carbon Sphere")
+                    isHeld = (GameObject)Instantiate(Carbon, new Vector3(2, 1, 3), Quaternion.identity);
+            }
+        }
+
+        if (OVRInput.GetUp(OVRInput.Button.One))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                Debug.Log("RIP MY HEAD");
+
+                if (isHeld.gameObject.name.Contains("Hydrogen") && hit.transform.name == ("Hydrogen Atom"))
+                {
+                    isTargetted = (GameObject)hit.transform.gameObject;
+                    isTargetted.GetComponent<Renderer>().material = hydrogenMat;
+                    Debug.Log("Hydrogen Molecule Completed");
+                    Destroy(isHeld);
+                }
+                else if (isHeld.gameObject.name.Contains("Nitrogen") && hit.transform.name == ("Nitrogen Atom"))
+                {
+                    isTargetted = (GameObject)hit.transform.gameObject;
+                    isTargetted.GetComponent<Renderer>().material = nitrogenMat;
+                    Debug.Log("Nitrogen Molecule Completed");
+                    Destroy(isHeld);
+                }
+                else if (isHeld.gameObject.name.Contains("Oxygen") && hit.transform.name == ("Oxygen Atom"))
+                {
+                    isTargetted = (GameObject)hit.transform.gameObject;
+                    isTargetted.GetComponent<Renderer>().material = oxygenMat;
+                    Debug.Log("Nitrogen Molecule Completed");
+                    Destroy(isHeld);
+                }
+                else if (isHeld.gameObject.name.Contains("Carbon") && hit.transform.name == ("Carbon Atom"))
+                {
+                    isTargetted = (GameObject)hit.transform.gameObject;
+                    isTargetted.GetComponent<Renderer>().material = carbonMat;
+                    Debug.Log("Nitrogen Molecule Completed");
+                    Destroy(isHeld);
+                }
+                else
+                {
+                    Destroy(isHeld);
+                }   
+
+            }
+        }   
+    }
 }
