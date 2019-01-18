21f2eb7aff4569dfe1217444005b27156189d91d
Jon <premiumjon13@gmail.com>
2019-01-18 14:28:45 -0500

    Laser Maybe?
    Tried the laser

---
+++ SpaceBake/Assets/Laser.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Laser : MonoBehaviour
+{
+    private LineRenderer ray;
+
+    // Use this for initialization
+	void Start ()
+	{
+	    ray = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    Transform rightHandAnchor; // Assign to the proper transform
+	    Ray pointer = new Ray(ray.transform.position, ray.transform.forward);
+
+	    ray.SetPosition(0, pointer.origin);
+	    ray.SetPosition(1, pointer.origin + pointer.direction * 500.0f);
+
+    }
+}
