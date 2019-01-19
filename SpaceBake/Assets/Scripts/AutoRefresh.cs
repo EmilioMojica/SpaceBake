83c907492a0fd09a8624a7c235c829089a232036
Jon <premiumjon13@gmail.com>
2019-01-19 09:21:24 -0500

    AutoRefresh

---
+++ SpaceBake/Assets/Scripts/AutoRefresh.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AutoRefresh : MonoBehaviour
+{
+
+    private float _timeToReset = 45;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    _timeToReset -= Time.deltaTime;
+
+	    if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) || OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger))
+	    {
+	        _timeToReset = 45;
+	    }
+
+        if (_timeToReset <= 0)
+            SceneManager.LoadScene("SpaceShuttle");
+    }
+}
