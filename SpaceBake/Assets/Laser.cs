c57d57c1237a6a5b71f880cf0a33eda153fe9516
Crucidian <Crucidian@users.noreply.github.com>
2019-01-18 19:12:58 -0500

    Reverting the laser to its previous version

--- SpaceBake/Assets/Laser.cs
+++ SpaceBake/Assets/Laser.cs
@@ -16,15 +16,9 @@ public class Laser : MonoBehaviour
 	void Update () {
 	    Transform rightHandAnchor; // Assign to the proper transform
 	    Ray pointer = new Ray(ray.transform.position, ray.transform.forward);
-        RaycastHit hit;
-        ray.SetPosition(0, pointer.origin);
-
-        // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(transform.position, transform.forward, out hit, 100.0f))
-        {
-            ray.SetPosition(1,hit.point);
-
-        }
+       
+	    ray.SetPosition(0, pointer.origin);
+	    ray.SetPosition(1, pointer.origin + pointer.direction * 1f);
 
     }
 }
